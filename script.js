65cde25aafa4ec33ab45db0af85300008118160c
Rahul Vishwakarma <103241598+rahulvishwakarma0111@users.noreply.github.com>
2023-03-26 23:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-window.promises = [];
+window.promises=[];
+window.promises = Array.from({ length: 5 }, () => new Promise(resolve => {
+  const timeout = Math.floor(Math.random() * 5) + 1;
+  setTimeout(() => resolve(`Promise resolved after ${timeout} seconds`), timeout * 1000);
+}));
 
-// Do not change the code above this
-// add your promises to the array `promises`
+Promise.any(window.promises)
+  .then(result => document.getElementById("output").innerHTML = result);
